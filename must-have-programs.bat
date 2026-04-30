454885361fec008432d78b97a2bdaf68eb97dd77
Yan Campêlo das Chagas <yanchagas2004@gmail.com>
2026-04-30 05:24:15 -0300

    feat: configs do windows

---
+++ must-have-programs.bat
@@ -0,0 +1,28 @@
+@REM Drivers e Utilitários
+winget install Wacom.WacomTabletDriver
+winget install 7zip.7zip
+winget install Logitech.GHUB
+winget install Logitech.OptionsPlus
+winget install OBSProject.OBSStudio
+winget install Discord.Discord
+winget install Hibbiki.Chromium
+winget install Blip
+winget install shinchiro.mpv
+
+@REM Softwares e Linguagens de Programação
+winget install Microsoft.PowerShell
+winget install Git.Git
+winget install Github.cli
+winget install Microsoft.WSL
+winget install Microsoft.VisualStudioCode
+winget install Golang.Go
+winget install Python.Python.3.12
+winget install Python.Python
+winget install Postman.Postman
+winget install Insomnia.Insomnia
+
+@REM Jogos
+winget install EpicGames.EpicGamesLauncher
+winget install Valve.Steam
+winget instal GameSir.GameSirConnect
+winget instal Asus.ArmouryCrate
